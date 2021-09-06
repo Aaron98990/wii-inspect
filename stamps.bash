8cb860bc6568f6e129885c15fb0cc40088c792bf
Aaron Feleke <aaronfeleke@gmail.com>
2021-09-06 02:05:02 -0500

    Better file verification

--- stamps.bash
+++ stamps.bash
@@ -1,11 +1,15 @@
 #!/bin/bash                                                       
-
-if [[ ! -d 00010000525a5445 ]]; then
-	echo "Running tachtig. Make sure tachtig executable is in your path."
-	tachtig data.bin
-fi
-
-if [ "$1" == "" ]; then
+# Exits program if first argument is empty
+if [ -z "$1" ]; then
+	echo bash stamps.bash FILE
+	echo FILE should be: Sports2.dat OR data.bin
+	exit 1
+# Exits program if file does not exist.
+elif [ ! -f "$1" ]; then
+	    echo "$1: No such file"
+	    exit 2
+elif [ "$1" == "data.bin" ]; then
+tachtig "$1"
 hex=$(xxd -p -c 1000000 00010000525a5445/Sports2.dat)
 else
 hex=$(xxd -p -c 1000000 $1)
@@ -38,8 +42,7 @@ echo $BIN
 
 twos() { x=$((16#$1)); [ "$x" -gt 32767 ] && ((x=x-65536)); printf "%+d " $x; }
 OneDate() {
-printf "%s:\t" "$1" 
-x=`doc $2`
+x=`doc $1`
 name_dec_pos=$((x+44))
 hiOrRec=$((x+828))
 new=$((x+832))
@@ -47,6 +50,9 @@ old=$((x+840))
 min=${hex:x:8}
 x=$((x-16))
 y=$((x+28))
+z=$((x+36))
+a=$((x+44))
+figureout=$((x+860))
 if [ "${hex:x:8}" == "55705363" ] && [ "${hex:hiOrRec:4}" == "0000" ]; then
 #readable="UpSc"
 readable="Wii Record            "
@@ -56,40 +62,57 @@ readable="Personal Record       "
 elif [ "${hex:x:8}" == "47556e69" ]; then
 #readable="GUni"
 readable="Stamp                 "
+elif [ "${hex:x:8}" == "4d646c45" ] && [ "${hex:figureout:20}" == "00000000000300000001" ]; then
+#readable="Mdle"
+echo "${hex:figureout:8}"  #I got it 4/18/21 - the 0001 is the important part
+readable="All Pro Class (May be Wrong)"
 elif [ "${hex:x:8}" == "4d646c45" ] ; then
 #readable="Mdle"
+echo "${hex:figureout:20}"
 readable="Pro or Superstar Class"
 elif [ "${hex:x:8}" == "506c6179" ]; then
 readable="Last Sport Played     "
 else
 readable=${hex:x:8}
 fi
-if [ "${hex:y:4}" == "0001" ]; then
+if [ "${hex:y:4}" == "0001" ] &&  [ "${hex:a:4}" == "0000" ]; then
+sportin="Power Cruising - Beach"
+elif [ "${hex:y:4}" == "0001" ]; then
 sportin="Power Cruising"
 elif [ "${hex:y:4}" == "0000" ]; then
 sportin="Swordplay"
 elif [ "${hex:y:4}" == "0002" ]; then
 sportin="Archery"
 elif [ "${hex:y:4}" == "0003" ]; then
-sportin="???"
+sportin="???-Is a sport"
+elif [ "${hex:y:4}" == "0004" ] && [ "${hex:a:4}" == "0002" ]; then
+	sportin="Basketball - 3 Point Contest"
 elif [ "${hex:y:4}" == "0004" ]; then
 sportin="Basketball"
+elif [ "${hex:y:4}" == "0005" ] && [ "${hex:z:4}" == "0002" ]; then
+	sportin="Bowling - Standard"
+elif [ "${hex:y:4}" == "0005" ] && [ "${hex:z:4}" == "0004" ]; then
+	sportin="Bowling - Spin Control"
 elif [ "${hex:y:4}" == "0005" ]; then
 	sportin="Bowling"
 elif [ "${hex:y:4}" == "0006" ]; then
 sportin="Canoeing"
 elif [ "${hex:y:4}" == "0007" ]; then
 sportin="Table Tennis"
+elif [ "${hex:y:4}" == "0008" ] && [ "${hex:a:4}" == "0000" ]; then
+sportin="Wakeboarding - Beginner"
 elif [ "${hex:y:4}" == "0008" ]; then
 sportin="Wakeboarding"
 elif [ "${hex:y:4}" == "0009" ]; then
-sportin="???"
+sportin="???-Is a sport"
 elif [ "${hex:y:4}" == "000a" ]; then
 sportin="Golf"
 elif [ "${hex:y:4}" == "000b" ]; then
 sportin="Frisbee Golf"
 elif [ "${hex:y:4}" == "000c" ]; then
 sportin="Cycling"
+elif [ "${hex:y:4}" == "000d" ] && [ "${hex:z:4}" == "0001" ]; then
+	sportin="Air Sports - Skydiving"
 elif [ "${hex:y:4}" == "000d" ]; then
 	sportin="Air Sports"
 elif [ "${hex:y:4}" == "000e" ]; then
@@ -113,12 +136,6 @@ if [ "${hex:x:8}" == "4d646c45" ]; then
 printf "%s %s %s" ${hex:new:4} "<-" ${hex:old:4}
 fi
 echo
-}
-TimesPlayed() {
-printf "%s:\t" "$1" 
-x=`doc $2`
-min=${hex:$x:2}
-echo `hexToDec $min`
 }
 sport() {
 printf "%s:\t" "$1" 
@@ -214,10 +231,10 @@ sport "Golf               " "8e68"
 sport "Canoeing           " "8e7c"
 sport "Cycling            " "8ea4"
 sport "Skydiving          " "8eb8"
-echo $countStamps "/100" "Stamps total for above"
+echo $countStamps "/100" "Stamps total for above player"
 countStamps=0
 echo
-z=`doc 90b6`
+z=`doc 90b4`
 echo `hexToAscii ${hex:z:30}`
 sport "Showdown           " "9af4"
 sport "Swordplay Duel     " "9b08"
@@ -240,8 +257,8 @@ sport "Canoeing           " "9c84"
 sport "Cycling            " "9cac"
 sport "Skydiving          " "9cc0"
 
-echo $countStamps "/100" "Stamps for above player"
 countStamps=0
+echo $countStamps "/100" "Stamps total for above player"
 echo
 z=`doc 9ea8`
 echo `hexToAscii ${hex:z:30}`
@@ -266,79 +283,38 @@ sport "Canoeing           " "aa64"
 sport "Cycling            " "aa8c"
 sport "Skydiving          " "aaa0"
 
-echo $countStamps "This is countStamps Total"
+echo $countStamps "/100" "Stamps total for above player"
 countStamps=0
 echo
 # For more players, get the last player and add 0xDE4 to all values.
 
 # 25 Records of Recent history
 echo "25 Recent Plays - Instead of clicking A & B to start game, just wait to see the plays or use the + or - button the Wiimote."
-OneDate "Skydiving          " "4e24"
-OneDate "Skydiving          " "4e34"
-OneDate "Skydiving          " "4ffc"
-OneDate "Skydiving          " "51c4"
-OneDate "Skydiving          " "538c"
-OneDate "Skydiving          " "5554"
-OneDate "Skydiving          " "571c"
-OneDate "Skydiving          " "58e4"
-OneDate "Skydiving          " "5aac"
-OneDate "Skydiving          " "5c74"
-OneDate "Skydiving          " "5e3c"
-OneDate "Skydiving          " "6004"
-OneDate "Skydiving          " "61cc"
-OneDate "Skydiving          " "6394"
-OneDate "Skydiving          " "655c"
-OneDate "Skydiving          " "6724"
-OneDate "Skydiving          " "68ec"
-OneDate "Skydiving          " "6ab4"
-OneDate "Skydiving          " "6c7c"
-OneDate "Skydiving          " "6e44"
-OneDate "Skydiving          " "700c"
-OneDate "Skydiving          " "71d4"
-OneDate "Skydiving          " "739c"
-OneDate "Skydiving          " "7564"
-OneDate "Skydiving          " "772c"
-OneDate "Skydiving          " "78f4"
+OneDate "4e24"
+OneDate "4e34"
+OneDate "4ffc"
+OneDate "51c4"
+OneDate "538c"
+OneDate "5554"
+OneDate "571c"
+OneDate "58e4"
+OneDate "5aac"
+OneDate "5c74"
+OneDate "5e3c"
+OneDate "6004"
+OneDate "61cc"
+OneDate "6394"
+OneDate "655c"
+OneDate "6724"
+OneDate "68ec"
+OneDate "6ab4"
+OneDate "6c7c"
+OneDate "6e44"
+OneDate "700c"
+OneDate "71d4"
+OneDate "739c"
+OneDate "7564"
+OneDate "772c"
+OneDate "78f4"
 
 
-echo "Game Plays. Restarting or quitting does not count."
-TimesPlayed "Dsadsa                    " "4d73"
-TimesPlayed "Dsadsa                    " "4d77"
-TimesPlayed "Dsadsa                    " "4d7b"
-TimesPlayed "Standard Bowling          " "4d7f"
-TimesPlayed "Dsadsa                    " "4d83"
-TimesPlayed "Dsadsa                    " "4d87"
-TimesPlayed "Dsadsa                    " "4d8b"
-TimesPlayed "Dsadsa                    " "4d8f"
-TimesPlayed "Dsadsa                    " "4d93"
-TimesPlayed "Dsadsa                    " "4d97"
-TimesPlayed "Dsadsa                    " "4d9b"
-TimesPlayed "Dsadsa                    " "4d9f"
-TimesPlayed "Dsadsa                    " "4da3"
-TimesPlayed "Dsadsa                    " "4da7"
-TimesPlayed "Dsadsa                    " "4dab"
-TimesPlayed "Dsadsa                    " "4daf"
-TimesPlayed "Cycling                   " "4db3"
-TimesPlayed "Skydiving                 " "4db7"
-TimesPlayed "Dsadsa                    " "4dbb"
-TimesPlayed "Dsadsa                    " "4dbf"
-TimesPlayed "Dsadsa                    " "4dc3"
-TimesPlayed "Dsadsa                    " "4dc7"
-TimesPlayed "Dsadsa                    " "4dcb"
-TimesPlayed "Dsadsa                    " "4dcf"
-TimesPlayed "Archery                   " "4dd3"
-TimesPlayed "Dsadsa                    " "4dd7"
-TimesPlayed "3 Point Contest           " "4ddb"
-TimesPlayed "Dsadsa                    " "4ddf"
-TimesPlayed "Dsadsa                    " "4de3"
-TimesPlayed "Dsadsa                    " "4de7"
-TimesPlayed "Dsadsa                    " "4deb"
-TimesPlayed "Dsadsa                    " "4def"
-TimesPlayed "Dsadsa                    " "4df3"
-TimesPlayed "Swordplay-Duel?           " "4df7"
-TimesPlayed "Dsadsa                    " "4dfb"
-TimesPlayed "Golf                      " "4dff"
-TimesPlayed "Island Flyover            " "4e03"
-TimesPlayed "Dsadsa                    " "4e07"
-TimesPlayed "Swordplay-Duel?           " "4e0b"
-TimesPlayed "Dsadsa                    " "4e0f"
