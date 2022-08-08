0eee6afa57ced000ace50df8a9a9a73e06c8f43f
Aaron Feleke <aaronfeleke@gmail.com>
2022-08-07 22:58:38 -0500

    Remove extra files

--- highScores.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-# binFile = open('Resort202105082205/Sports2.dat')
-proc = subprocess.Popen(["tachtig", "data.bin"] )
-output = proc.communicate()[0]
-print(output)
-# proc = subprocess.Popen(["xxd", "-p", "Resort202105082205/Sports2.dat"] )
-# output = proc.communicate()[0]
-# print(output)
